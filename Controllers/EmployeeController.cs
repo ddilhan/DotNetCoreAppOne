9462fc270e7d65e09654a0097ce5d9c3ab24615a
ddilhan <47178813+ddilhan@users.noreply.github.com>
2020-01-15 12:31:22 +0530

    commit without  bin and obj

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestCoreAppOne.Models;
+
+namespace TestCoreAppOne.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Employee _employee;
+
+        public EmployeeController(Models.Employee employee)
+        {
+            _employee = employee;
+        }
+        public IActionResult Index()
+        {
+            return View(_employee.GetEmployees());
+        }
+
+        [HttpGet]
+        public IActionResult EditEmployee(int id) 
+        {
+            return View(_employee.getEmployee(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditEmployee( Employee emp)
+        {
+            _employee.updateEmployee(emp);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteEmployee(int id) 
+        {
+            _employee.deleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult AddEmployee(Employee emp)
+        {
+            _employee.addEmployee(emp);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCoreAppOne.Models
+{
+    public class Employee
+    {
+        private AppDBContext _context;
+        public Employee()
+        {
+
+        }
+
+        public Employee(AppDBContext context)
+        {
+            _context = context;
+        }
+        public int Id { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Location { get; set; }
+        [BindProperty]
+        public int Age { get; set; }
+
+        public Employee getEmployee(int id)
+        {
+            var employee = _context.EmployeeTable.Find(id);
+            return employee;
+        }
+
+        public Employee addEmployee(Employee employee)
+        {
+            _context.Add(employee);
+            _context.SaveChanges();
+            return employee;
+        }
+
+        public Employee deleteEmployee(int id) 
+        {
+            var employee = _context.EmployeeTable.Find(id);
+            if (employee != null)
+            {
+                _context.Remove(employee);
+                _context.SaveChanges();
+            }
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            var employees = _context.EmployeeTable;
+            return employees;
+        }
+
+        public Employee updateEmployee(Employee employee)
+        {
+            var employees = _context.EmployeeTable.Attach(employee);
+            employees.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return employee;
+        }
+    }
+}
